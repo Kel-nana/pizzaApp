fecaad09d161c73cb20e1b72cb769ae67096adac
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-27 16:38:14 +0300

    initial commit

---
+++ src/main/java/com/kel/pizzaapp/PizzaappApplication.java
@@ -0,0 +1,34 @@
+package com.kel.pizzaapp;
+
+import com.kel.pizzaapp.config.PizzaConfig;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class PizzaappApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PizzaappApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		final PizzaConfig pizzaConfig = new PizzaConfig(
+				"tomato", "mozzarela", "thin");
+		log.info (
+				String.format("I want a %s crust pizza, with%s and %s sauce",
+						pizzaConfig.getCrust(),
+						pizzaConfig.getTopping(),
+						pizzaConfig.getSauce()
+				));
+
+	}
+
+
+	}
+
+
